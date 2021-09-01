64b9e042dfa08971cf01aa5af56061a35a83e496
imaqn <ima54a@gmail.com>
2021-09-01 14:48:11 +0700

    add affine encrypt

--- main.py
+++ main.py
@@ -4,26 +4,42 @@ import math
 def at_bash(str):
     return str[::-1]
 
-def affine(str):
+
+def affineEncrypt(text, a, b):
     encrypted = ""
-    key1 = 17
-    key2 = 20
     for i in range(len(str)):
         if str[i] != ' ':
-            encrypted = encrypted + chr((((key1*(ord(str[i])-ord('A')) + key2))%26)+ord('A'))
+            encrypted = encrypted + chr(((a * (ord(text[i]) - ord('A')) + b) % 26) + ord('A'))
         else:
-            encrypted = encrypted + str[i]
-    print("encrypted")
+            encrypted = encrypted + text[i]
+    print(encrypted)
     return encrypted
+
+
+def affineDecrypt(text, a, b):
+    inv_a = 0
+    for i in range(26):
+        flag = (a * i) % 26
+        if flag == 1:
+            inv_a = i
+    decrypted = ""
+    for i in range(len(text)):
+        if text[i] != ' ':
+            decrypted = decrypted + chr((inv_a * (ord(text[i]) + ord('A') - b) % 26) + ord('A'))
+        else:
+            decrypted = decrypted + text[i]
+    print(decrypted)
+    return decrypted
+
 
 def caesarEncrypt(text, shift):
     encrypted = ""
     for i in range(len(text)):
-        if(ord(text[i])+int(shift)>90):
-            encrypted = encrypted + chr(ord(text[i])+int(shift)-26)
+        if (ord(text[i]) + int(shift) > 90):
+            encrypted = encrypted + chr(ord(text[i]) + int(shift) - 26)
         else:
-            encrypted = encrypted + chr(ord(text[i])+int(shift))
-            
+            encrypted = encrypted + chr(ord(text[i]) + int(shift))
+
     print(encrypted)
     return encrypted
 
@@ -31,19 +47,20 @@ def caesarEncrypt(text, shift):
 def caesarDecrypt(text, shift):
     decrypted = ""
     for i in range(len(text)):
-        if(ord(text[i])-int(shift)<65):
-            decrypted = decrypted + chr(ord(text[i])-int(shift)+26)
+        if (ord(text[i]) - int(shift) < 65):
+            decrypted = decrypted + chr(ord(text[i]) - int(shift) + 26)
         else:
-            decrypted = decrypted + chr(ord(text[i])-int(shift))
-            
+            decrypted = decrypted + chr(ord(text[i]) - int(shift))
+
     print(decrypted)
     return decrypted
+
 
 def columnarEncrypt(text, key):
     encrypted = ""
 
     # Create and fill array with "_"
-    rows, cols = (len(key), math.ceil(len(text)/len(key)))
+    rows, cols = (len(key), math.ceil(len(text) / len(key)))
     arr = [["_" for i in range(rows)] for j in range(cols)]
 
     # Structure:
@@ -56,10 +73,10 @@ def columnarEncrypt(text, key):
     for i in range(cols):
         for j in range(rows):
             try:
-                arr[i][j] = text[j+(i*(rows))]
+                arr[i][j] = text[j + (i * (rows))]
             except IndexError:
                 pass
-    
+
     # Create list representing string:key as list of ascii
     keyList = []
     for i in range(len(key)):
@@ -67,7 +84,7 @@ def columnarEncrypt(text, key):
 
     # Convert ascii to sorted number for future exporting
     for i in range(len(key)):
-        keyList[keyList.index(min(j for j in keyList if j>(i-1)))] = i
+        keyList[keyList.index(min(j for j in keyList if j > (i - 1)))] = i
 
     # Add character to encrypted, per column based on keylist
     for j in range(rows):
@@ -76,15 +93,16 @@ def columnarEncrypt(text, key):
                 encrypted = encrypted + arr[i][keyList[j]]
             except IndexError:
                 pass
-    
+
     print(encrypted)
-    return(encrypted)
+    return (encrypted)
+
 
 def columnarDecrypt(text, key):
     decrypted = ""
 
     # Create and fill array with null value
-    rows, cols = (len(key), int(len(text)/len(key)))
+    rows, cols = (len(key), int(len(text) / len(key)))
     arr = [["" for i in range(rows)] for j in range(cols)]
 
     # Create list representing string:key as list of ascii
@@ -94,14 +112,14 @@ def columnarDecrypt(text, key):
 
     # Convert ascii to sorted number for future exporting
     for i in range(len(key)):
-        keyList[keyList.index(min(j for j in keyList if j>(i-1)))] = i
+        keyList[keyList.index(min(j for j in keyList if j > (i - 1)))] = i
 
     # Add character to array based on key
     for j in range(rows):
         for i in range(cols):
             try:
-                arr[i][keyList[j]] = text[i+(j*(rows))]
-                print(text[i+(j*(rows))])
+                arr[i][keyList[j]] = text[i + (j * (rows))]
+                print(text[i + (j * (rows))])
             except IndexError:
                 pass
 
@@ -109,19 +127,22 @@ def columnarDecrypt(text, key):
     for i in range(cols):
         for j in range(rows):
             decrypted = decrypted + arr[i][j]
-    
+
     print(decrypted)
-    return(decrypted)
+    return (decrypted)
+
 
 def encrypt(str):
-    print(affine(str))
+    print('Test')
+
 
 def decrypt(str):
-    print('Has not been implemented')
+    print('Test')
+
 
 def main():
     str = input('Enter the string: ')
-    opt = input('Choose 1 to encrypt or 2 to decrypt')
+    opt = input('Choose 1 to encrypt or 2 to decrypt: ')
     if opt == '1':
         encrypt(str)
     elif opt == '2':
